bf075051e1f30eb79515a932d9c5bf37644711ee
rahuls569 <79099957+rahuls569@users.noreply.github.com>
2023-02-12 11:23:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,41 @@
-'''Train CIFAR10 with PyTorch.'''
+import albumentations as A
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
-
 import torchvision
 import torchvision.transforms as transforms
+import cv2 
 
-import os
-#import argparse
+augmentations = [A.HorizontalFlip(), A.ShiftScaleRotate(),
+                 A.CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1, min_height=16,
+                                 min_width=16, fill_value=(0.5, 0.5, 0.5), mask_fill_value=None)]
 
-#from models import *
-#from utils import progress_bar
+def load_cifar10(root, augmentations=None):
+  train_transforms = transforms.Compose([
+                                    #  transforms.Resize((28, 28)),
+                                   #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+                                 
+                                     transforms.ToTensor(),
+                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) 
+                                     # Note the difference between (0.1307) and (0.1307,)
+                                    ])
 
+  test_transforms = transforms.Compose([
+                                     transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) 
+                                     ])
 
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+  trainset = torchvision.datasets.CIFAR10(root=root, train=True, download=True, transform=train_transforms)
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+  if augmentations is not None:
+      def augment(image):
+          image = transforms.ToPILImage()(image)
+          for aug in augmentations:
+              image = aug(image=image)["image"]
+          image = transforms.ToTensor()(image)
+          return image
+      trainset.Transform = augment
+    
+  testset = torchvision.datasets.CIFAR10(root=root, train=False, download=True, transform=test_transforms)
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+  classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
-
-
+  return trainset, testset, classes

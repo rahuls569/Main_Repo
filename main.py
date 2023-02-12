14295f2e6795575499d68373bde74f5875d77822
rahuls569 <79099957+rahuls569@users.noreply.github.com>
2023-02-12 13:44:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,44 +7,43 @@ import os
 import torch.nn.functional as F
 import cv2
 
-from albumentations import Compose, PadIfNeeded, RandomCrop, Normalize, HorizontalFlip, ShiftScaleRotate, CoarseDropout, Cutout
-from torchvision import transforms
-
-augmentations = [HorizontalFlip(), ShiftScaleRotate(),
-                 CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1, min_height=16,
+augmentations = [A.HorizontalFlip(), A.ShiftScaleRotate(),
+                 A.CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1, min_height=16,
                                  min_width=16, fill_value=(0.5, 0.5, 0.5), mask_fill_value=None)]
 
-def augment(image, target):
-    image = transforms.ToPILImage()(image)
-    for aug in augmentations:
-        image = aug(image=image)["image"]
-    image = transforms.ToTensor()(image)
-    return image, target
+def load_cifar10(root, augmentations=None):
+  train_transforms = transforms.Compose([
+                                    #  transforms.Resize((28, 28)),
+                                   #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+                                     transforms.RandomCrop(32, padding=4),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                                     # Note the difference between (0.1307) and (0.1307,)
+                                    ])
 
-def load_cifar10(root, augment=False):
-    train_transforms = transforms.Compose([
-        PadIfNeeded(40),
-        RandomCrop(32,32),
-        HorizontalFlip(),
-        Cutout(num_holes=1, max_h_size=16, max_w_size=16, fill_value=[0.4914*255, 0.4822*255, 0.4471*255], always_apply=True, p=1.00),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+  test_transforms = transforms.Compose([
+                                     transforms.ToTensor(),
+                                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                                     ])
 
-    test_transforms = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+  trainset = torchvision.datasets.CIFAR10(root=root, train=True, download=True, transform=train_transforms)
 
-    trainset = torchvision.datasets.CIFAR10(root=root, train=True, download=True, transform=train_transforms)
-    if augment:
-        trainset.transform = augment
+  if augmentations is not None:
+      def augment(image):
+          image = transforms.ToPILImage()(image)
+          for aug in augmentations:
+              image = aug(image=image)["image"]
+          image = transforms.ToTensor()(image)
+          return image
+      trainset.Transform = augment
+    
+  testset = torchvision.datasets.CIFAR10(root=root, train=False, download=True, transform=test_transforms)
 
-    testset = torchvision.datasets.CIFAR10(root=root, train=False, download=True, transform=test_transforms)
+  classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+  return trainset, testset, classes
 
-    return trainset, testset, classes
 import torch
 import torchvision
 import torchvision.transforms as transforms

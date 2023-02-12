eecc4be3393c7dcb3b58874f5e1cf6436eb470ff
rahuls569 <79099957+rahuls569@users.noreply.github.com>
2023-02-12 11:43:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,3 +39,22 @@ def load_cifar10(root, augmentations=None):
   classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
   return trainset, testset, classes
+
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+def setup_dataloaders(trainset, testset, SEED, Batch):
+   
+    cuda = torch.cuda.is_available()
+    torch.manual_seed(SEED)
+    if cuda:
+        torch.cuda.manual_seed(SEED)
+    
+    dataloader_args = dict(shuffle=True, batch_size=Batch, num_workers=2, pin_memory=True) if cuda else dict(shuffle=True, batch_size=Batch)
+    
+    train_loader = torch.utils.data.DataLoader(trainset, **dataloader_args)
+    test_loader = torch.utils.data.DataLoader(testset, **dataloader_args)
+    
+    return train_loader, test_loader 

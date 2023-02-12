f5979f8e6fafdbeb115bf1acda1bf66e664832d7
rahuls569 <79099957+rahuls569@users.noreply.github.com>
2023-02-12 15:17:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -73,6 +73,44 @@ class YourClass:
         for i in range(32):
             index = labels[i].item()
             print('Image %d: %s' % (i, self.classes[index]))
-            
-            
+  
+
+
+
+def graph_loss_accuracy(Trainer1, tester, EPOCHS):
+
+    fig, axs = plt.subplots(2, 2, figsize=(15, 12))
+
+    # train_epoch_linspace = np.linspace(1, EPOCHS, len(Trainer1.train_losses))
+    # test_epoch_linspace = np.linspace(1, EPOCHS, len(tester.test_losses))
+
+    # Training Plot
+    axs[0,0].plot(Trainer1.train_losses, color='r', label='Training Loss')
+    axs[0,0].set_xlabel('Epochs')
+    axs[0,0].set_ylabel('Training Loss')
+    axs[0,0].set_title('Training Loss vs. Epochs')
+    axs[0,0].legend()
+
+    # TEST Loss Plot
+    axs[0,1].plot(tester.test_losses, color='r', label='Test Loss')
+    axs[0,1].set_xlabel('Epochs')
+    axs[0,1].set_ylabel('Test Loss')
+    axs[0,1].set_title('Test Loss vs. Epochs')
+    axs[0,1].legend()
+
+    # TRAIN Accuracy Plot
+    axs[1,0].plot( Trainer1.train_acc, color='r', label='Training Accuracy')
+    axs[1,0].set_xlabel('Epochs')
+    axs[1,0].set_ylabel('Training Accuracy')
+    axs[1,0].set_title('Training Accuracy vs. Epochs')
+    axs[1,0].legend()
+
+    # TEST Accuracy Plot
+    axs[1,1].plot( tester.test_acc, color='r', label='Test Accuracy')
+    axs[1,1].set_xlabel('Epochs')
+    axs[1,1].set_ylabel('Test Accuracy')
+    axs[1,1].set_title('Test Accuracy vs. Epochs')
+    axs[1,1].legend()
+
+    plt.show()           
 
